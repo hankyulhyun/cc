eaf99824b2e899875b0489dec895f6e76a255cae
HanKyul, Hyun <hyunhankyul@gmail.com>
2018-10-09 22:12:00 +0900

    First implement of cc py

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+
+FROM ubuntu:18.04
+MAINTAINER hyunhankyul@gmail.com
+
+RUN apt-get update && \
+    apt-get install -y python3-pip && \
+    apt-get install -y python3-dev && \
+    apt-get install -y build-essential
+
+WORKDIR /app
+COPY . /app
+
+RUN pip3 install -r requirements.txt
+
+EXPOSE 5000
+
+ENTRYPOINT [ "python3" ]
+CMD ["cc.py"]
---
+++ cc.py
@@ -0,0 +1,41 @@
+
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import send_from_directory
+
+import numpy as np
+import cv2
+
+from time import sleep
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+@app.route('/req_cc', methods=['POST'])
+def req_cc():
+    message = request.form['message']
+    app.logger.debug('Message : ' + message)
+
+    img = np.zeros((1080, 1920, 3), np.uint8)
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    text = message
+    
+    textsize = cv2.getTextSize(text, font, 1, 2)[0]
+    app.logger.debug('Text size : (%d, %d)' % (textsize[0], textsize[1]))
+
+    textX = int((img.shape[1] - textsize[0]) / 2)
+    textY = int((img.shape[0] - textsize[1]) / 2)
+    app.logger.debug('Write position : (%d, %d)' % (textX, textY))
+
+    cv2.putText(img, text, (textX, textY), font, 1, (255, 255, 255), 2)
+
+    cv2.imwrite('hello_image.jpg', img)
+    return send_from_directory('/home/hankyul/project/cc_python/', 'hello_image.jpg', as_attachment=True)
+
+
+if __name__=='__main__':
+    app.run(debug=True, host='0.0.0.0')
